7b6d0a61333d72c77f75f9effcf530d5924d9acd
GhidraDragon <bo@shangsoftware.ru>
2025-02-21 13:44:34 -0500

    chrome

--- install_python.sh
+++ install_python.sh
@@ -4,4 +4,12 @@ sudo apt install python3.11
 sudo apt install python3.11-venv
 python3.11 -m venv ~/venv-metal
 source ~/venv-metal/bin/activate
-python -m pip install -U pip
+python3.11 -m pip install -U pip
+
+python3.11 pip install -r requirements.txt
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://storage.googleapis.com/chrome-for-testing-public/133.0.6943.98/linux64/chromedriver-linux64.zip
+sudo apt install unzip
+unzip chromedriver-linux64.zip
+mv chromedriver-linux64/chromedriver . && chmod +x chomedriver
